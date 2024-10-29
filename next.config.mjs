be18caf7146dd3a6a53ef0f0badd85e99517d1ee
Kevin Bench <hisense363@proton.me>
2024-10-29 15:45:09 -0400

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return []; // Remove rewrites since we're handling in middleware
-  },
 };
 
 export default nextConfig;
