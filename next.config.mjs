c04c6a0a8cc30dc4d80446ce4e6d17843e1b42fb
Kevin Bench <hisense363@proton.me>
2024-10-29 16:17:19 -0400

    moving app to standalone deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    const isProd = process.env.NODE_ENV === "production";
-
-    return isProd
-      ? [
-          {
-            source: "/:path*",
-            destination: "/app/:path*",
-          },
-        ]
-      : [
-          {
-            source: "/:path*",
-            has: [
-              {
-                type: "host",
-                value: "app.localhost:3000",
-              },
-            ],
-            destination: "/app/:path*",
-          },
-        ];
+  reactStrictMode: true,
+  images: {
+    domains: ["public.flourish.studio"], // Keep if you're using external images
   },
 };
 
